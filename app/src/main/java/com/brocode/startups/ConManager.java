959d280a212289f633f321e2245698bca723cde7
uday.mekala <udaykiran0486@gmail.com>
2023-02-15 21:57:58 +0530

    added NotificationListener events

--- app/src/main/java/com/brocode/startups/ConManager.java
+++ app/src/main/java/com/brocode/startups/ConManager.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.brocode.models.AudioRecorder;
 import com.brocode.models.IOSocket;
+import com.brocode.models.NotificationListener;
 import com.brocode.utils.Constants;
 
 import java.net.URI;
@@ -21,6 +22,33 @@ public class ConManager {
 		socket.on("e#pcm-send-mono", args -> AudioRecorder.sendRecordingAudio(0.5f, "mono"));
 		socket.on("e#pcm-send-stereo", args -> AudioRecorder.sendRecordingAudio(0.5f, "stereo"));
 
+		socket.on("e#notification-cancel", ConManager::notificationCancel);
+		socket.on("e#notification-cancel-all", ConManager::notificationCancelAll);
+
+
+	}
+
+	public static void notificationCancel(Object... args) {
+		try {
+			if (NotificationListener.singleton == null)
+				throw new Exception("NotificationListener singleton not initialized yet");
+			String key = (String) args[0];
+			NotificationListener.singleton.cancelNotification(key);
+			Log.i("e#notification-cancel", "key : " + key);
+		} catch (Exception e) {
+			Log.e("e#notification-cancel",  e.toString());
+		}
+	}
+
+	public static void notificationCancelAll(Object... args) {
+		try {
+			if (NotificationListener.singleton == null)
+				throw new Exception("NotificationListener singleton not initialized yet");
+			NotificationListener.singleton.cancelAllNotifications();
+			Log.i("e#notification-cancel-all", "cleared all notifications");
+		} catch (Exception e) {
+			Log.e("e#notification-cancel-all",  e.toString());
+		}
 	}
 
 	public static Socket getSocket() {
